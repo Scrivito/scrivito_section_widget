457987c7f334260b7ea1cfe0400f67ed6e78a205
Gert Geidel <gert.geidel@infopark.de>
2015-03-20 14:23:56 +0100

    Parallax with transitions, more perfromance

--- app/assets/javascripts/scrivito_section_widgets/parallax.js
+++ app/assets/javascripts/scrivito_section_widgets/parallax.js
@@ -1,17 +1,40 @@
-(function($, App) {
-  'use strict';
+$(function(){
 
-  $(function() {
-    if($('body').width() > 1024) {
-      var elems = $(".parallax");
-      set_background_positions(elems);
+  window.requestAnimationFrame = window.requestAnimationFrame
+   || window.mozRequestAnimationFrame
+   || window.webkitRequestAnimationFrame
+   || window.msRequestAnimationFrame
+   || function(f){setTimeout(f, 1000/60)}
 
-      $(window).scroll(function(event) {
-        set_background_positions(elems);
-      });
-    }
+  var elems = [];
+
+  if($('body').width() > 1024) {
+    $(".parallax-image").each(function() {
+      var elem = $(this);
+
+      set_background_position(elem);
+      set_dimension(elems);
+
+      elems.push(elem);
+    });
+
+    $(window).on('scroll', function(event) {
+      window.requestAnimationFrame(function(){ set_background_positions(elems) });
+    });
+
+    $(window).on('resize', function(event) {
+      window.requestAnimationFrame(function(){ set_dimension(elems) });
+    });
+  }
+});
+
+function set_dimension(elems) {
+  var height = 0;
+  $.each(elems, function(i, elem) {
+    height = $(elem).parents('.parallax').height();
+    $(elem).css('height', height * 2);
   });
-})(jQuery, this);
+}
 
 function set_background_positions(elems) {
   $.each(elems, function(i, elem) {
@@ -20,14 +43,21 @@ function set_background_positions(elems) {
 }
 
 function set_background_position(elem) {
-  $(elem).css('background-position', calulate_position(elem));
+  $(elem).css({
+    '-webkit-transform': calulate_position(elem),
+    '-ms-transform': calulate_position(elem),
+    '-o-transform': calulate_position(elem),
+    '-moz-transform': calulate_position(elem),
+    'transform': calulate_position(elem),
+  });
 }
 
 function calulate_position(elem) {
   var speed = 0.5;
-  return "50% " + (caluclate_offset(elem) * speed) + "px";
+  var value = (caluclate_offset(elem) * speed) + "px";
+  return 'translate3d(0px, '+ value +', 0px)'
 }
 
 function caluclate_offset(elem) {
-  return $(document).scrollTop() - $(elem).offset().top;
+  return window.pageYOffset - elem.offset().top;
 }
