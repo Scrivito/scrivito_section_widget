46b79d452997a92b602bec4b9c4978bb50c6d862
Gert Geidel <gert.geidel@infopark.de>
2015-06-11 16:15:56 +0200

    Fix an js error when not logged in to scrivito - register mime type to contentbrowser throws an error on console if not logged in to scrivito

--- app/assets/javascripts/scrivito_section_widgets/video_content_browser.js
+++ app/assets/javascripts/scrivito_section_widgets/video_content_browser.js
@@ -1,7 +1,13 @@
-$(function() {
-  scrivito.register_default_obj_class_for_content_type({
-    'video/mp4': 'Video',
-    'video/ogg': 'Video',
-    'video/webm': 'Video'
-  })
-});
+(function($, App) {
+  'use strict';
+
+  scrivito.on('content', function() {
+    if(scrivito.in_editable_view()) {
+      scrivito.register_default_obj_class_for_content_type({
+        'video/mp4': 'Video',
+        'video/ogg': 'Video',
+        'video/webm': 'Video'
+      });
+    }
+  });
+})(jQuery, this);
