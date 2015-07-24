e8f7a68199da81795d09fbb70a49acf511bf243c
Gert Geidel <gert.geidel@infopark.de>
2015-07-24 14:51:55 +0200

    Speed for parallax image

--- app/assets/javascripts/scrivito_section_widgets/parallax.js
+++ app/assets/javascripts/scrivito_section_widgets/parallax.js
@@ -6,30 +6,32 @@ $(function(){
    || window.msRequestAnimationFrame
    || function(f){setTimeout(f, 1000/60)}
 
-  var elems = $(".parallax-image");
+  scrivito.on('content', function() {
+    var images = $(".parallax-image");
 
-  if($('body').width() > 1024) {
-    set_background_positions(elems);
+    if($('body').width() > 1024) {
+      set_background_positions(images);
 
-    $(window).on('scroll', function(event) {
-      window.requestAnimationFrame(function(){ set_background_positions(elems); });
+      $(window).on('scroll', function(event) {
+        window.requestAnimationFrame(function(){ set_background_positions(images); });
+      });
+    } else {
+      set_dimension(images);
+    }
+
+    $(window).on('resize', function(event) {
+      set_dimension(images);
     });
-  } else {
-    set_dimension(elems);
-  }
-
-  $(window).on('resize', function(event) {
-    set_dimension(elems);
   });
 });
 
-function set_dimension(elems) {
+function set_dimension(images) {
   var height = 0;
   var container = null;
 
-  $.each(elems, function(i, elem) {
-    container = $(elem).parents('.parallax');
-    height = $(elem).get(0).clientHeight / 1.3;
+  $.each(images, function(i, image) {
+    container = $(image).parents('.parallax');
+    height = $(image).get(0).clientHeight / 1.3;
 
     if(height > 0) {
       container.css({
@@ -39,28 +41,38 @@ function set_dimension(elems) {
   });
 }
 
-function set_background_positions(elems) {
-  $.each(elems, function(i, elem) {
-    set_background_position(elem);
+function set_background_positions(images) {
+  $.each(images, function(i, image) {
+    set_background_position(image);
   });
 }
 
-function set_background_position(elem) {
-  $(elem).css({
-    '-webkit-transform': calulate_position(elem),
-    '-ms-transform': calulate_position(elem),
-    '-o-transform': calulate_position(elem),
-    '-moz-transform': calulate_position(elem),
-    'transform': calulate_position(elem),
-  });
+function set_background_position(image) {
+  var value = $(image).data('parallax-speed');
+  if(!(value == "fixed" || value == "deactivate")) {
+    var data = calulate_position(image, value);
+    $(image).css({
+      '-webkit-transform': data,
+      '-ms-transform': data,
+      '-o-transform': data,
+      '-moz-transform': data,
+      'transform': data,
+    });
+  }
 }
 
-function calulate_position(elem) {
-  var speed = 0.5;
-  var value = (caluclate_offset(elem) * speed) + "px";
+function calulate_position(image, value) {
+  var speed = data_to_speed(value);
+  var value = (caluclate_offset(image) * speed) + "px";
   return 'translate3d(0px, '+ value +', 0px)'
 }
 
-function caluclate_offset(elem) {
-  return window.pageYOffset - $(elem).offset().top;
+function caluclate_offset(image) {
+  return window.pageYOffset - $(image).offset().top;
+}
+
+function data_to_speed(value) {
+  if(value == "fast") return 0.6;
+  if(value == "slow") return 0.1;
+  return 0.3;
 }
--- app/models/section_parallax_widget.rb
+++ app/models/section_parallax_widget.rb
@@ -2,4 +2,5 @@ class SectionParallaxWidget < Widget
   attribute :section_content, :widgetlist
   attribute :background_image, :reference
   attribute :height, :string
+  attribute :speed, :enum, values: ["fixed", "slow", "medium", "fast", "deactivate"], default: "medium"
 end
