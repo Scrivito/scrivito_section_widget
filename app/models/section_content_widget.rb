5663cb27defede0b29c3584663c140db279bfcba
Gert Geidel <gert.geidel@infopark.de>
2017-06-22 14:45:22 +0200

    Style attribute for section content

--- app/models/section_content_widget.rb
+++ app/models/section_content_widget.rb
@@ -2,12 +2,21 @@ class SectionContentWidget < Widget
   attribute :section_content, :widgetlist
   attribute :background_color, :string
   attribute :padding_size, :enum, values: ['none', 'small', 'medium', 'large'], default: 'medium'
+  attribute :styles, :stringlist
 
   def selectable_color_classes
     if Obj.respond_to?('selectable_color_classes')
       Obj.selectable_color_classes('section_content_widget', 'background_color')
     else
       SectionContentWidget.fallback_colors
+    end
+  end
+
+  def scrivito_selectable_style_classes
+    if Obj.respond_to? 'scrivito_selectable_style_classes'
+      Obj.scrivito_selectable_style_classes('SectionContentWidget')
+    else
+      ['border-top','border-bottom']
     end
   end
 
